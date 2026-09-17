670241e287731c9cc832f1b4a4648e1a81d0efb3
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-17 21:36:09 +0300

    added view and endpoint

--- musician/serializers.py
+++ musician/serializers.py
@@ -3,9 +3,13 @@ from rest_framework import serializers
 from musician.models import Musician
 
 
-class MusicianListRetrieveSerializer(serializers.ModelSerializer):
-    date_of_applying = serializers.DateField(read_only=True, required=False)
-    is_adult = serializers.BooleanField(read_only=True, required=False)
+class MusicianSerializer(serializers.ModelSerializer):
+    date_of_applying = serializers.DateField(
+        read_only=True, required=False
+    )
+    is_adult = serializers.BooleanField(
+        read_only=True, required=False
+    )
 
     class Meta:
         model = Musician
@@ -18,12 +22,3 @@ class MusicianListRetrieveSerializer(serializers.ModelSerializer):
             "date_of_applying",
             "is_adult"
         )
-
-# class MusicianSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Musician
-#         fields = (            "id",
-#             "first_name",
-#             "last_name",
-#             "instrument",
-#             "age", )
